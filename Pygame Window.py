742108f276acb6bfa5b2a07128b68db720e7f8a2
rutba333 <sa2497589@gmail.com>
2024-12-26 20:42:07 +0600

    activity1

---
+++ Pygame Window.py
@@ -0,0 +1,10 @@
+#import necessary libraries
+from tkinter import*
+
+#create window
+window=Tk()
+
+#set window title and geomatry
+window.title('Demo window')
+window.geometry('400x300')
+window.mainloop()
